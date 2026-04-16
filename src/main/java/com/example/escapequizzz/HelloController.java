041686da1e6ab7acb8fc2ee42fa9fb51bc7d08f3
guiarthur7 <arthur.guillaume@ynov.com>
2026-04-16 17:20:09 +0200

    Création du front pour les dialogue et du back

--- src/main/java/com/example/escapequizzz/HelloController.java
+++ src/main/java/com/example/escapequizzz/HelloController.java
@@ -1,14 +1,45 @@
 package com.example.escapequizzz;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import java.io.IOException;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Label emilien;
+
+    private String[] dialogues = {
+            "Emilien : Tu vas devoir répondre à des questions de culture générale",
+            "Emilien : Si tu réussi à répondre à 5 questions consécutives j'irais désamorcer la bombe",
+            "Emilien : Jean-Luc compte sur toi !"
+    };
+
+    private int indexDialogue = 0;
+
+    @FXML
+    protected void Suivant() {
+        if (indexDialogue < dialogues.length) {
+            emilien.setText(dialogues[indexDialogue]);
+            indexDialogue++;
+        } else {
+            changerDePage("quizz-view.fxml");
+        }
+    }
+
+    private void changerDePage(String fxmlFile) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(fxmlFile));
+            Scene nextScene = new Scene(fxmlLoader.load());
+            Stage stage = (Stage) emilien.getScene().getWindow();
+
+            stage.setScene(nextScene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
